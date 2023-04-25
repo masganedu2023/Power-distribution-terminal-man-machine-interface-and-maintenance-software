43dc5d922b68803af4a555e5de028e20eb8eae58
masganedu2023 <130138037+masganedu2023@users.noreply.github.com>
2023-04-25 10:54:31 +0900

    Create BaseUserControl.cs
    BaseUserControl

---
+++ BaseUserControl.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Power2000.CommuCfg
+{
+    public partial class BaseUserControl : UserControl
+    {
+        public BaseUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
